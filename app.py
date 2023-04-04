327e466a833b630bcace187d00df6efb8a5082a7
David Kovacs <davi.kovacs.1991@gmail.com>
2023-04-04 10:34:37 +0200

    started implementing user management

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from pymongo import MongoClient
 
 import json
@@ -16,25 +16,48 @@ def create_app():
     client = MongoClient(db_uri)
     app.db = client.techieblog
 
+    app.secret_key = 'FDrNddE0Zhih-OgTYoOHBcMfmXO9iuIH-vl2Jlw6usI'
+    users = {}
+
     @app.route("/", methods=['GET', 'POST'])
     def home():
         if request.method == 'POST':
             entry_content = request.form.get('content')
             date = datetime.today().strftime('%Y-%m-%d')
-            app.db.entries.insert_one({'content': entry_content, 'date': date})
+            app.db.entries.insert_one({'content': entry_content, 'date': date, 'user': session.get('user')})
 
         entries_with_date = [
             (
                 entry['content'],
+                entry['user'],
                 entry['date'],
                 datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
             )
             for entry in app.db.entries.find({})
         ]
-        return render_template("posts.html", main='posts', entries=entries_with_date)
+        return render_template("posts.html", main='posts', entries=entries_with_date, user=session.get('user'))
 
-    @app.route('/calendar')
-    def calendar():
-        return render_template('calendar.html', main='calendar')
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        return render_template('login.html', main='login')
+    
+    @app.route('/logout', methods=['GET', 'POST'])
+    def logout():
+        if request.method == 'POST':
+            session['user'] = None
+            redirect('/', 200)  # not working
+            
+        return render_template('logout.html', main='login')
+    
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+            
+            users[username] = password
+            session['user'] = username
+            
+        return render_template('signup.html', main='signup')
 
     return app
